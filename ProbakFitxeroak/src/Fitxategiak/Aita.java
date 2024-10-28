5eb8be116c79884d51deba2d851b21d92b4053d8
ioritzlope45@gmail.com <ioritzlope45@gmail.com>
2024-10-28 09:18:21 +0100

    Identatuta

--- ProbakFitxeroak/src/Fitxategiak/Aita.java
+++ ProbakFitxeroak/src/Fitxategiak/Aita.java
@@ -7,43 +7,40 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Aita {
-    public static void main(String[] args) {
-        try {
-            // Semearen exekutablearen ruta osoa
-        	  String childJarPath = "C:\\Users\\2ag3.iorilope\\Desktop\\aleatorio.jar";
+	public static void main(String[] args) {
+		try {
+			// Semearen exekutablearen ruta osoa
+			String childJarPath = "C:\\Users\\2ag3.iorilope\\Desktop\\aleatorio.jar";
 
-              // Eekutatu fitxaetgia
-              Process process = Runtime.getRuntime().exec("java -jar \"" + childJarPath + "\"");
+			// Exekutatu fitxaetgia
+			Process process = Runtime.getRuntime().exec("java -jar \"" + childJarPath + "\"");
 
-         
-            BufferedReader childOutput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            PrintWriter childInput = new PrintWriter(new OutputStreamWriter(process.getOutputStream()), true);
+			BufferedReader childOutput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			PrintWriter childInput = new PrintWriter(new OutputStreamWriter(process.getOutputStream()), true);
 
-            Scanner scanner = new Scanner(System.in);
-            String input;
+			Scanner scanner = new Scanner(System.in);
+			String input;
 
-            System.out.println("Zerbait idatzi ausazko zenbaki bat eskatzeko 'amaiera' amaitzeko:");
+			System.out.println("Zerbait idatzi ausazko zenbaki bat eskatzeko 'amaiera' amaitzeko:");
 
-        
-            while (!(input = scanner.nextLine()).equalsIgnoreCase("amaiera")) {
-                childInput.println("generar"); 
-                childInput.flush();
+			while (!(input = scanner.nextLine()).equalsIgnoreCase("amaiera")) {
+				childInput.println("sortu");
+				childInput.flush();
 
-                // Irakurri eta erakutsi ausazko zenbakia
-                String randomNumber = childOutput.readLine();
-                if (randomNumber != null) {
-                    System.out.println("Lortutako ausazko zenbakia: " + randomNumber);
-                }
-            }
+				// Irakurri eta erakutsi ausazko zenbakia
+				String randomNumber = childOutput.readLine();
+				if (randomNumber != null) {
+					System.out.println("Lortutako ausazko zenbakia: " + randomNumber);
+				}
+			}
 
-           
-            childInput.println("amaiera");
-            childInput.flush();
-            process.waitFor();
+			childInput.println("amaiera");
+			childInput.flush();
+			process.waitFor();
 
-            System.out.println("Prozesua amaitu da.");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+			System.out.println("Prozesua amaitu da.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- ProbakFitxeroak/src/Fitxategiak/Aleatorio.java
+++ ProbakFitxeroak/src/Fitxategiak/Aleatorio.java
@@ -6,23 +6,23 @@ import java.io.PrintWriter;
 import java.util.Random;
 
 public class Aleatorio {
-    public static void main(String[] args) {
-        Random random = new Random();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter writer = new PrintWriter(System.out, true);
+	public static void main(String[] args) {
+		Random random = new Random();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter writer = new PrintWriter(System.out, true);
 
-        try {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (line.equalsIgnoreCase("amaiera")) {
-                    break;
-                }
-                int randomNumber = random.nextInt(11);
-                writer.println(randomNumber);
-                writer.flush();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		try {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				if (line.equalsIgnoreCase("amaiera")) {
+					break;
+				}
+				int randomNumber = random.nextInt(11);
+				writer.println(randomNumber);
+				writer.flush();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
